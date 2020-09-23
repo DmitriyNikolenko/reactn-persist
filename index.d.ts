406938b72c05f4835ff3036a23ca75ab662d2d37
Dmitriy Nikolenko <dmitriy@MacBook-Pro-Dmitriy.local>
2020-09-23 09:36:46 +0300

    fix: fix typing declarations

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,19 @@
 declare module 'reactn-persist' {
+	import * as React from 'react'
+	
 	interface TStorageInstance {
 		getItem: (key: string) => any;
-		setItem: (key: string, persistedGlobalValue: object | any[]) => void;
+		setItem: (key: string, value: string) => void;
 	}
 
 	interface TInitOptions {
 		storage: TStorageInstance;
-		key: string;
-		whitelist: string[];
-		debounceDelay: number;
-		debug: boolean;
-		initialValue: object | any[];
-		provider: any;
+		key?: string;
+		whitelist?: string[];
+		debounceDelay?: number;
+		debug?: boolean;
+		initialValue?: object | any[];
+		provider?: React.Component<{}, {}>;
 	}
 
 	const init: (options: TInitOptions) => void;
